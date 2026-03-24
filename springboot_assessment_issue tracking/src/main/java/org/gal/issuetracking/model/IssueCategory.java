eedebd98412624415d4e24b331921eef7f84b053
Ishita <ishitachauhan530@gmail.com>
2026-03-24 15:08:54 +0530

    Delete springboot_assessment_issue tracking directory

--- springboot_assessment_issue tracking/src/main/java/org/gal/issuetracking/model/IssueCategory.java
+++
@@ -1,9 +0,0 @@
-package org.gal.issuetracking.model;
-
-public enum IssueCategory {
-
-    ACCESS_PERMISSION,
-    CLOUD_ISSUE,
-    DESKTOP_ISSUE,
-    OTHER_ISSUE
-}
--- springboot_assessment_issue tracking/src/main/java/org/gal/issuetracking/repository/EmployeeRepository.java
+++
@@ -1,7 +0,0 @@
-package org.gal.issuetracking.repository;
-
-import org.gal.issuetracking.model.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-}
